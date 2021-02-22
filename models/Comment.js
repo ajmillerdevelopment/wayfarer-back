07bc70e13931880c59cbff10b33d433a3f50222a
Alex Miller <ajm9594@gmail.com>
2021-02-22 14:08:38 -0800

    models setup

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema ({
-
-});
-
+    body: {type: String, required: true},
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+}, {timestamps: true});
+const Comment = mongoose.model('Comment', commentSchema)
 module.exports = Comment;
