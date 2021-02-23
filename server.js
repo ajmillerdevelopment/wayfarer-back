30accf53804377b1ac8e07dce64f64bf3446beb1
Alex Miller <ajm9594@gmail.com>
2021-02-23 11:41:24 -0800

    CORS hotfix

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const bodyParser = require('body-parser')
 const routes = require('./routes');
 const PORT = process.env.PORT || 4000;
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/city', routes.cities)
