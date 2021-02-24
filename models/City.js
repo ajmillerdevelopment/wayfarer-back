38a16c088bc6c406dd062b0980e128c8688aec46
Alex Miller <ajm9594@gmail.com>
2021-02-24 14:53:04 -0800

    added image field for cities

--- models/City.js
+++ models/City.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const citySchema = new mongoose.Schema({
     name: {type: String, required: true, trim: true},
-    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+    image: {url: String}
 });
 const City = mongoose.model('City', citySchema)
 module.exports = City;
